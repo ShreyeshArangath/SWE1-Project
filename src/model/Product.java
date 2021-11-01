6ac53de142eca0c4c668f80e2f6019dbec2f313a
Shreyesh Arangath <shreyesh@Shreyeshs-MacBook-Pro.local>
2021-11-01 16:46:15 -0500

    base structure

---
+++ src/model/Product.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author shreyesh
+ */
+public class Product {
+    public String itemNumber; 
+    public String itemDescription;
+    public Double retailPrice; 
+    public Double discountPercentage; 
+    public Integer availableUnits; 
+    public Boolean needsRestock;
+    
+    public Product(String itemNumber, String itemDescription, Double retailPrice, 
+            Double discountPercentage, int availableUnits, boolean needsRestock){
+        this.itemDescription = itemDescription;
+        this.itemNumber = itemNumber; 
+        this.retailPrice = retailPrice; 
+        this.discountPercentage = discountPercentage; 
+        this.availableUnits = availableUnits; 
+        this.needsRestock = needsRestock;
+    }
+    
+    public String getItemNumber(){
+        return this.itemNumber;
+    }
+    
+     public String getItemDescription(){
+        return this.itemDescription;
+    }
+    
+}
